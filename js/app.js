16adc6fb5f067ba45ebc5a8fba58066239cd1162
Sean Chain <seanchain2.0@gmail.com>
2015-04-27 13:04:24 +0800

    add data binding

--- js/app.js
+++ js/app.js
@@ -77,4 +77,19 @@ app.directive('userlinks', function($compile){
     }
 });
 
+app.directive('ngInitial', function() {
+    return {
+        restrict: 'A',
+        controller: [
+            '$scope', '$element', '$attrs', '$parse', function($scope, $element, $attrs, $parse) {
+                var getter, setter, val;
+                val = $attrs.ngInitial || $attrs.value;
+                getter = $parse($attrs.ngModel);
+                setter = getter.assign;
+                setter($scope, val);
+            }
+        ]
+    };
+});
+
 
